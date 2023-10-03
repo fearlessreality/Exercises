81826a3121e8270c4b00863e3fd8464a9f25b3dc
fearlessreality <pol1997@abv.bg>
2023-10-03 18:37:43 +0300

    Update 01. Sum Digits.js

--- Exercise - Data Types and Variables/01. Sum Digits.js
+++ Exercise - Data Types and Variables/01. Sum Digits.js
@@ -1,1 +1,11 @@
- 
+function sumDigits(input) {
+  let stringInput = input.toString();
+  let sum = 0;
+  for (let i = 0; i < stringInput.length; i++) {
+    sum += Number(stringInput[i]);
+  }
+  console.log(sum);
+}
+sumDigits(245678);
+sumDigits(97561);
+sumDigits(543);
