7668c975e87b91e34589e72b062efb6d8adc66d6
fearlessreality <pol1997@abv.bg>
2023-10-21 22:09:28 +0300

    Update 01. Smallest of Three Numbers.js

--- Exercise - Functions/01. Smallest of Three Numbers.js
+++ Exercise - Functions/01. Smallest of Three Numbers.js
@@ -1,11 +1,13 @@
-// function smallestOfThreeNumbers(num1, num2, num3) {
-//   let min = Math.min(num1, num2, num3);
-//   console.log(min);
-// }
-// smallestOfThreeNumbers(2, 5, 3);
-// smallestOfThreeNumbers(600, 342, 123);
-// smallestOfThreeNumbers(25, 21, 4);
-// smallestOfThreeNumbers(2, 2, 2);
+function smallestOfThreeNumbers(num1, num2, num3) {
+  let min = Math.min(num1, num2, num3);
+  console.log(min);
+}
+smallestOfThreeNumbers(2, 5, 3);
+smallestOfThreeNumbers(600, 342, 123);
+smallestOfThreeNumbers(25, 21, 4);
+smallestOfThreeNumbers(2, 2, 2);
+
+// --------------------------------------------------------------
 
 let min = (num1, num2, num3) => Math.min(num1, num2, num3);
 console.log(min(2, 5, 3));
