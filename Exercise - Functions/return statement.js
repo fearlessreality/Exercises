900c9252f451a20b3f4317bb25838387a1b5e1e5
fearlessreality <pol1997@abv.bg>
2023-10-22 12:36:24 +0300

    Update 02. Add and Subtract.js

--- Exercise - Functions/return statement.js
+++ Exercise - Functions/return statement.js
@@ -1,5 +1,5 @@
 function readFullName(firstName, lastName) {
   return firstName + " " + lastName; //immediately stop the function's execution and RETURNS the specified value to the caller
-}
+} //Everything outside the function is not part of the function.
 const fullName = readFullName("John", "Smith"); //we write the name into a variable; const - cannot be reassigned
 console.log(fullName);
