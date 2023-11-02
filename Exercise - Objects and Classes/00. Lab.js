0bf285962028584400cd1a0291e5e83d79f30eca
fearlessreality <pol1997@abv.bg>
2023-11-02 19:48:30 +0200

    Update 00. Lab.js

--- Exercise - Objects and Classes/00. Lab.js
+++ Exercise - Objects and Classes/00. Lab.js
@@ -13,3 +13,17 @@ function personInfo(firstName, lastName, age) {
 
 personInfo("Peter", "Pan", "20");
 personInfo("George", "Smith", "18");
+//------------------------------------------------------
+let myArr = [
+  ["polly", 26],
+  ["chris", 32],
+];
+
+myArr.forEach(([name, age], index) => {
+  console.log(name, age, index);
+});
+
+const anotherArr = ["polly", 26];
+
+let [name, age] = anotherArr;
+//------------------------------------------------------
