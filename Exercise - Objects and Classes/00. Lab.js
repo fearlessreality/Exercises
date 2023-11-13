68664a0913c93dc48e3fb18993d9e4b0be278086
fearlessreality <pol1997@abv.bg>
2023-11-13 17:55:34 +0200

    Update 00. Lab.js

--- Exercise - Objects and Classes/00. Lab.js
+++ Exercise - Objects and Classes/00. Lab.js
@@ -27,3 +27,31 @@ const anotherArr = ["polly", 26];
 
 let [name, age] = anotherArr;
 //------------------------------------------------------
+
+let polly = {
+  name: { firstName: "Paola", middleName: "Veselinova", lastName: "Tosheva" },
+  age: 26,
+  height: { cm: 165, mm: 1165, inches: 345 },
+  eyes: { count: 2, colour: "brown", type: "hooded" },
+};
+let chris = {
+  name: {
+    firstName: "Christian",
+    middleName: "Sergeev",
+    lastName: "Sergienko",
+  },
+  age: 32,
+  height: { cm: 174, mm: 1174, inches: 555 },
+  eyes: { count: 2, colour: "blue", type: "hooded" },
+};
+// let arr = [polly, chris];
+let obj = { polly, chris };
+let arr = Object.values(obj);
+function sortByHeight(a, b) {
+  return b.height.cm - a.height.cm;
+}
+arr.sort(sortByHeight);
+function printName({ firstName, middleName, lastName }) {
+  console.log(`The tallest person is: ${firstName} ${middleName} ${lastName}.`);
+}
+printName(arr[0].name);
