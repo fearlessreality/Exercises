9e8f51c9eea45e7a5949a414c114c75a3bf544cf
fearlessreality <pol1997@abv.bg>
2023-11-29 12:48:03 +0200

    Update 02. Odd Occurrences.js

--- Exercise - Associative Arrays/02. Odd Occurrences.js
+++ Exercise - Associative Arrays/02. Odd Occurrences.js
@@ -1,14 +1,33 @@
+// function oddOccurrences(string) {
+//   let arr = string.toLowerCase().split(" ");
+//   // console.log(arr);
+//   let counts = {};
+//   arr.forEach((word) => {
+//     counts[word] = (counts[word] || 0) + 1;
+//   });
+//   let toExtract = Object.entries(counts)
+//     .filter(([, value]) => value % 2 !== 0)
+//     .map(([key]) => key)
+//     .join(" "); // filter creates an array with the values that meet the condition
+//   // map creates a new array with the given elements from the first array
+//   console.log(toExtract);
+// }
+// oddOccurrences("Cake IS SWEET is Soft CAKE sweet Food");
+
 function oddOccurrences(string) {
-  let sentence = string.toLowerCase().split(" ");
-  //   console.log(sentence);
+  let arr = string.toLowerCase().split(" ");
+  // console.log(arr);
   let counts = {};
-  sentence.forEach((word) => {
+  arr.forEach((word) => {
     counts[word] = (counts[word] || 0) + 1;
   });
-  let filtered = Object.entries(counts)
-    .filter(([, value]) => value % 2 !== 0)
-    .map(([k]) => k)
-    .join(" "); // map creates a new array with the given elements from the first array
-  console.log(filtered);
+  let toExtract = Object.entries(counts)
+    .filter(([, value]) => {
+      return value % 2 !== 0; // Added return statement here
+    })
+    .map(([key]) => key)
+    .join(" "); // filter creates an array with the values that meet the condition
+  // map creates a new array with the given elements from the first array
+  console.log(toExtract);
 }
-oddOccurrences("Java C# Php PHP Java PhP 3 C# 3 1 5 C#");
+oddOccurrences("Cake IS SWEET is Soft CAKE sweet Food");
